5cb25cc54c029ecdd76c4f56bf892a2a8a9d644e
christiano112 <enyiachristian12@gmail.com>
2024-01-25 22:25:46 +0100

    init project

--- src/root.component.tsx
+++ src/root.component.tsx
@@ -1,5 +1,8 @@
 export default function Root(props) {
-  return (<section>{props.name} is mounted!
-  <h1>This is the Dashboard Page</h1>
-  </section>);
+  return (
+    <section>
+      {props.name} is mounted!
+      <h1>This is the Dashboard Page</h1>
+    </section>
+  );
 }
